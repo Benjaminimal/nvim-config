d6e78e00e4df8bef0bc3fecbaf713fd9f92b0014
Benjamin Zinschitz <benjamin@formunauts.at>
2025-01-13 12:57:58 +0100

    fix python formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,7 +3,7 @@ return {
     "conform.nvim",
     opts = {
       formatters_by_ft = {
-        python = { "ruff_organize_imports" },
+        python = { "ruff_organize_imports", "ruff_format" },
       },
     },
   },
