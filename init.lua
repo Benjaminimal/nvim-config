d41043a7dbeb39a3659363eef6b93d3926c2d57d
Benjamin Zinschitz <benjamin@formunauts.at>
2025-01-19 14:18:11 +0100

    add keymaps for next and prev loclist item

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@
 --    | || |_| | |_| | |_| |
 --    |_| \___/|____/ \___/
 --
---  * Add keymap for loclist next
 --  * Make trouble default for quickfix and loclist
 --  * Starting a test run should open summary
 --  * A finished test run shouldn't close the loclist
