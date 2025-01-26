237e35aedfd4d96dd0a045d324e383bb8dee0433
Benjamin Zinschitz <benjamin@formunauts.at>
2025-01-26 22:06:27 +0100

    show hidden files in explorer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -63,4 +63,16 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      },
+    },
+  },
 }
