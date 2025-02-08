c43dd1b9b586b088e302821453f6ba8894eb8a14
Benjamin Zinschitz <benjamin@formunauts.at>
2025-02-08 12:40:51 +0100

    use mini for surround

--- init.lua
+++ init.lua
@@ -12,17 +12,8 @@
 --  * copilot chat history?
 --  * look into this coloscheme https://github.com/rebelot/kanagawa.nvim
 --  * sort search (workspace) symbols results by length/match
---  * use mini for surround
---
---      Old text                    Command         New text
--- --------------------------------------------------------------------------------
---    surr*ound_words             ysiw)           (surround_words)
---    *make strings               ys$"            "make strings"
---    [delete ar*ound me!]        ds]             delete around me!
---    remove <b>HTML t*ags</b>    dst             remove HTML tags
---    'change quot*es'            cs'"            "change quotes"
---    <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
---    delete(functi*on calls)     dsf             function calls
+--  * keymap for evaluating arithmetic expressions in insert mode
+--  * neovim adds line breaks automatically leading to broken python syntax
 --
 -- ]
 
--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,12 +1,16 @@
 return {
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
   },
 }
