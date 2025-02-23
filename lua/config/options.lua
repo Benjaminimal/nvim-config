cb7dc2a5a05fd08cfd73bdc7433ee23a148230ce
Benjamin Zinschitz <benjamin@formunauts.at>
2025-02-23 20:27:53 +0100

    comment some options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- sane line breaks
 vim.opt.formatoptions = "jcrqln"
 
+-- enable editorconfig
 vim.g.editorconfig = true
 
+-- don't do animations
 vim.g.snacks_animate = false
 
 -- LSP Server to use for Python.
