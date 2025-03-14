6962bf4d0edf34770e6036c761f7d05f4a156676
Benjamin Zinschitz <benjamin@formunauts.at>
2025-03-14 10:57:06 +0100

    use mypy for typechecking

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -13,7 +13,7 @@ return {
                 -- typeCheckingMode = "strict",
                 -- NOTE: uncomment this to ignore linting. Good for projects where
                 -- basedpyright lights up as a christmas tree.
-                typeCheckingMode = "basic",
+                typeCheckingMode = "off",
                 ignore = { "*" },
                 autoSearchPaths = true,
                 useLibraryCodeForTypes = true,
@@ -30,5 +30,20 @@ return {
         },
       },
     },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, {
+        null_ls.builtins.diagnostics.mypy.with({
+          extra_args = function()
+            local virtual = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX") or "/usr"
+            return { "--python-executable", virtual .. "/bin/python" }
+          end,
+        }),
+      })
+    end,
   },
 }
