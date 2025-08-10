bb2b2022e724b4be77f47d6debd84e3271aedb9d
Benjamin Zinschitz <benjamin@formunauts.at>
2025-08-10 11:15:50 +0200

    customize edge colorscheme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -9,7 +9,18 @@ return {
   },
 
   -- add custom colorschemes
-  { "sainnhe/edge" },
+  {
+    "sainnhe/edge",
+    config = function()
+      vim.g.edge_style = "neon"
+      vim.g.edge_diagnostic_virtual_text = "colored"
+      vim.g.edge_current_word = "high contrast background"
+      vim.g.edge_inlay_hints_background = "dimmed"
+      vim.g.edge_colors_override = {
+        grey = { "#8790a0", "246" }, -- make code comments more visible
+      }
+    end,
+  },
   { "sainnhe/sonokai" },
   { "sainnhe/gruvbox-material" },
   { "sainnhe/everforest" },
