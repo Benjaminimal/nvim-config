b5e42e7ab4c48c10e42b9290ab13dae1bc7c8613
Benjamin Zinschitz <benjamin@formunauts.at>
2025-11-13 21:34:52 +0100

    chore(ai): add some keybinds

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,9 +1,17 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    keys = {
+      -- Toggle Chat
+      { "<leader>aa", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Chat Toggle" },
+      -- Actions picker
+      { "<leader>aA", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions" },
+      -- Paste selection to chat
+      { "<leader>aa", "<cmd>CodeCompanionChat Add<cr>", mode = { "v" }, desc = "Chat Add" },
+    },
+    opts = {},
   },
 }
