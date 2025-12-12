2cd56e06e99cc7fe411da767df831d487d725568
Benjamin Zinschitz <benjamin@formunauts.at>
2025-12-12 09:39:30 +0100

    feat(neotest): remove auto summary open

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,11 +1,3 @@
--- Open the summary panel after running `keymap_rhs`
-function summaryOpen(keymap_rhs)
-  return function()
-    keymap_rhs()
-    -- require("neotest").summary.open()
-  end
-end
-
 return {
   "nvim-neotest/neotest",
   dependencies = {
@@ -26,18 +18,5 @@ return {
       ["neotest-vitest"] = {},
     },
     status = { virtual_text = false },
-  },
-  -- stylua: ignore
-  keys = {
-    {"<leader>t", "", desc = "+test"},
-    { "<leader>tt", summaryOpen(function() require("neotest").run.run(vim.fn.expand("%")) end), desc = "Run File (Neotest)" },
-    { "<leader>tT", summaryOpen(function() require("neotest").run.run(vim.uv.cwd()) end), desc = "Run All Test Files (Neotest)" },
-    { "<leader>tr", summaryOpen(function() require("neotest").run.run() end), desc = "Run Nearest (Neotest)" },
-    { "<leader>tl", summaryOpen(function() require("neotest").run.run_last() end), desc = "Run Last (Neotest)" },
-    { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary (Neotest)" },
-    { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output (Neotest)" },
-    { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel (Neotest)" },
-    { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop (Neotest)" },
-    { "<leader>tw", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "Toggle Watch (Neotest)" },
   },
 }
