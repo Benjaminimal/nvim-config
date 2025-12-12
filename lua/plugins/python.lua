e167fa198834f46f8fccbf0f068742c1eed24dad
Benjamin Zinschitz <benjamin@formunauts.at>
2025-12-12 09:36:52 +0100

    refactor: move formatting specifics to python config

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -19,4 +19,12 @@ return {
       },
     },
   },
+  {
+    "conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", "ruff_format" },
+      },
+    },
+  },
 }
