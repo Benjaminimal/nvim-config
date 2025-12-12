5cf0bbb8e2d4597ca2375b35dd471fa42d11ec96
Benjamin Zinschitz <benjamin@formunauts.at>
2025-12-12 09:28:03 +0100

    fix(python): ensure pyrefly installed

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,12 +1,20 @@
 return {
   {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "pyrefly",
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         -- disable pyright / basedpyright
         pyright = { mason = false, autostart = false },
         basedpyright = { mason = false, autostart = false },
-        -- disable mypy LSP if you had one
+        -- disable mypy LSP
         mypy = { mason = false, autostart = false },
       },
     },
